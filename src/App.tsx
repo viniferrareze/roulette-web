259546642882838772151ed53278fdfb42c08350
Paulo Guilherme Santiago <37718188+Paulogsdv@users.noreply.github.com>
2020-05-20 22:57:40 -0300

    Feat: Adicionado css do bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
 import GlobalStyle from './styles/global';
+import './styles/bootstrap.min.css';
 
 const App: React.FC = () => (
    <BrowserRouter>
