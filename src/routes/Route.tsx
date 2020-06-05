b6049e86e72be6a44bcbbb033f91338a658c90b6
Carlos Vinícius Ferrareze <viniferrareze@gmail.com>
2020-06-04 23:37:39 -0300

    feat: removido comentario

--- src/routes/Route.tsx
+++ src/routes/Route.tsx
@@ -12,16 +12,6 @@ const Route: React.FC<RouteProps> = ({ isPrivate = false, component: Component, 
    const { user } = useAuth();
 
    return (
-      // <ReactDOMRoute
-      //    {...rest}
-      //    render={({ location }) => {
-      //       return isPrivate === !!user ? (
-      //          <Component />
-      //       ) : (
-      //          <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard', state: { from: location } }} />
-      //       );
-      //    }}
-      // />
       <ReactDOMRoute
          {...rest}
          render={() => {
